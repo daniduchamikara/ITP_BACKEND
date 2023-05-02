0573701d1dab2628eaf4f44d2cf460328e748ff3
dchamikara <dchamikara@iom.int>
2023-05-02 23:39:46 +0530

    schedule changes 2.0

--- backend/models/tripShedule.js
+++ backend/models/tripShedule.js
@@ -3,26 +3,27 @@ var Schema = mongoose.Schema;
 
 const userSchema = mongoose.Schema(
   {
-    name: {
+    customerName: {
       type: String,
-      required: [true, 'Please add a name'],
+      required: [true, 'Please add a customer name'],
     },
-    type: {
-      type: String,
-      required: [true, 'Please add a USER ROLE'],
-    },
-    number: {
-      type: String,
-      required: [true, 'Please add an email'],
-    },
-    status: {
-      type: String,
-      required: [true, 'Please add a password'],
-    },
-    driver: {
+    vehicleId: {
         type: String,
+      required: [true, 'Please add a vehicle Id'],
+    },
+    dateTime: {
+      type: String,
+      required: [true, 'Please add an date Time'],
     },
     driverName: {
+      type: String,
+      required: [true, 'Please add a driver Name'],
+    },
+    location: {
+        type: String,
+        required: [true, 'Please add a location'],
+    },
+    driverId: {
         type: String,
     },
   },
